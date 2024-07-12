70bab1a7a2a04459f17104a6be8fed38c011a5a8
= <=>
2024-07-12 18:15:48 +0530

    partial

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,12 +1,1 @@
-import { useState } from 'react'
 
-
-function App() {
-  
-
-  return (
-    <h1 className='text-4xl bg-yellow-400 text-center mt-10'>Hii Pinks</h1>
-  )
-}
-
-export default App
--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -1,10 +1,67 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import './index.css'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import Login from './components/Login/Login.jsx'
+import Getstarted from './components/Getstarted/Getstarted.jsx'
+import Gituser from './components/Gituser/Gituser.jsx'
+
+// const router = createBrowserRouter( [
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//           path: "",
+//           element: <Home />
+//       },
+//       {
+//           path: 'about',
+//           element: <About />
+//       },
+//       {
+//           path: 'contact',
+//           element: <Contact />
+//       },
+//       {
+//           path: 'login',
+//           element: <Login />
+//       },
+//       {
+//           path: 'start',
+//           element: <Getstarted />
+//       },
+//       {
+//           path: 'gituser/:username',
+//           element: <Gituser /> 
+//       }
+      
+//     ]
+
+//   }
+
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='login' element={<Login />} />
+      <Route path='start' element={<Getstarted />} />
+      <Route path='git/:username' element={<Gituser />} />
+
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider  router={router}/>
   </React.StrictMode>,
 )
